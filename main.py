684728d5aaf7e208180d321708700784008c2f2c
Lim-JL <limjiale.ljl@gmail.com>
2026-03-11 21:10:14 +0800

    refactored code for efficiency

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-import threading
 from pynput import keyboard
 from pygame import mixer
 
-SOUND_PATH = "fahhhhhhhhhhhhhh.mp3" 
 # you can realistically put any sound file here
-def play_sound():
-	mixer.init()
-	mixer.music.load(SOUND_PATH)
-	mixer.music.set_volume(0.3)
-	mixer.music.play()
+SOUND_PATH = "fahhhhhhhhhhhhhh.wav" 
+mixer.init()
+fahhh = mixer.Sound(SOUND_PATH)
+fahhh.set_volume(0.3)
+
 
 def on_press(key):
 	if key == keyboard.Key.esc:
 		return False
 	else:
-		threading.Thread(target=play_sound).start()
+		fahhh.play()
 
 with keyboard.Listener(on_press=on_press) as listener:
 	listener.join()
